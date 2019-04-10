64588d46f1ce1750d1f213486d0d18ec2b91359b
undx <egallois@talend.com>
2019-04-10 10:40:35 +0200

    chore(TDI-41957): add tests for AvroConverter

--- adlsgen2/src/test/java/org/talend/components/adlsgen2/common/format/avro/AvroConverterTest.java
+++ adlsgen2/src/test/java/org/talend/components/adlsgen2/common/format/avro/AvroConverterTest.java
@@ -14,36 +14,75 @@ package org.talend.components.adlsgen2.common.format.avro;
 
 import java.util.Arrays;
 import java.util.Date;
+import java.util.stream.Stream;
 
+import org.apache.avro.SchemaBuilder;
+import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.talend.components.adlsgen2.AdlsGen2TestBase;
+import org.talend.sdk.component.api.record.Record;
+import org.talend.sdk.component.api.record.Schema;
+import org.talend.sdk.component.api.record.Schema.Entry;
+import org.talend.sdk.component.api.record.Schema.Type;
 import org.talend.sdk.component.junit5.WithComponents;
 
+import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @WithComponents("org.talend.components.adlsgen2")
 class AvroConverterTest extends AdlsGen2TestBase {
 
     private AvroConverter converter = AvroConverter.of();
 
+    private GenericRecord avro;
+
     @BeforeEach
     protected void setUp() {
         super.setUp();
+
+        avro = new GenericData.Record( //
+                SchemaBuilder.builder().record("sample").fields() //
+                        .name("string").type().stringType().noDefault() //
+                        .name("int").type().intType().noDefault() //
+                        .name("long").type().longType().noDefault() //
+                        .name("double").type().doubleType().noDefault() //
+                        .name("boolean").type().booleanType().noDefault() //
+                        .endRecord());
+        avro.put("string", "a string sample");
+        avro.put("int", 710);
+        avro.put("long", 710L);
+        avro.put("double", 71.0);
+        avro.put("boolean", true);
     }
 
     @Test
     void of() {
+        assertNotNull(AvroConverter.of());
     }
 
     @Test
     void inferSchema() {
+        Schema s = converter.inferSchema(avro);
+        assertNotNull(s);
+        assertEquals(5, s.getEntries().size());
+        assertTrue(s.getType().equals(Type.RECORD));
+        assertTrue(s.getEntries().stream().map(Entry::getName).collect(toList())
+                .containsAll(Stream.of("string", "int", "long", "double", "boolean").collect(toList())));
     }
 
     @Test
     void toRecord() {
+        Record record = converter.toRecord(avro);
+        assertNotNull(record);
+        assertEquals("a string sample", record.getString("string"));
+        assertEquals(710, record.getInt("int"));
+        assertEquals(710L, record.getLong("long"));
+        assertEquals(71.0, record.getDouble("double"));
+        assertEquals(true, record.getBoolean("boolean"));
     }
 
     @Test
